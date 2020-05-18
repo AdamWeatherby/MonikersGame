fd343532f53dd5ccc8f037105b0fe2e2a42ab910
GrantJReid <gjreid1s@semo.edu>
2020-05-18 01:57:57 -0500

    Adding Spring Basics

---
+++ src/main/java/com/game/Monikers/MonikersApplication.java
@@ -0,0 +1,13 @@
+package com.game.Monikers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonikersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonikersApplication.class, args);
+	}
+
+}
