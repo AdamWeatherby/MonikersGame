400b6a56f3c3e05557f593aaf272bfbd5682439e
GrantJReid <gjreid1s@semo.edu>
2020-05-18 03:59:19 -0500

    grab random cards from top 500 pages

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import requests
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,4 +16,30 @@ def start():
 	print(request.args)
 	numberOfPlayers = request.args.get('p', default = 2, type = int)
 	print(numberOfPlayers)
+
+	numberOfCards = 10 * numberOfPlayers
+
+	cardsDict = []
+
+	S = requests.Session()
+
+	URL = "https://en.wikipedia.org/w/api.php"
+
+	PARAMS = {
+		"action": "query",
+		"format": "json", 
+		"list": "mostviewed", 
+		"pvimlimit": "500"
+	}
+
+	R = S.get(url=URL, params=PARAMS)
+	DATA = R.json()
+
+	MOSTVIEWED = DATA["query"]["mostviewed"]
+
+	for i in range(0, numberOfCards):
+		index = random.randint(2, 499)
+		cardsDict.append(MOSTVIEWED[index]["title"])
+		print(MOSTVIEWED[index]["title"])
+
 	return "Start game with " + str(numberOfPlayers) + " players"
